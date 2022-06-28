d0f398749e2cfd6e0a9cac26fd66148ff4b69d0f
linnos <39270663+linnos@users.noreply.github.com>
2022-06-28 09:11:41 -0400

    Cards are animation correctly on hover and on exit

--- Survival RPG/Assets/Scripts/OnHover.cs
+++ Survival RPG/Assets/Scripts/OnHover.cs
@@ -3,11 +3,18 @@ using UnityEngine.EventSystems;
 
 public class OnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    public Vector3 ogScale;
+    int id;
+
+    public void Start() {
+        transform.localScale = ogScale;
+    }
     //Detect if the Cursor starts to pass over the GameObject
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
         //Output to console the GameObject's name and the following message
         Debug.Log("Cursor Entering " + name + " GameObject");
+        id = LeanTween.scale(transform.gameObject, new Vector3(1.5f,1.5f,1.5f), 0.5f).setLoopPingPong().id;
     }
 
     //Detect when Cursor leaves the GameObject
@@ -15,5 +22,7 @@ public class OnHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
         //Output the following message with the GameObject's name
         Debug.Log("Cursor Exiting " + name + " GameObject");
+        LeanTween.cancel(id);
+        LeanTween.scale(transform.gameObject, ogScale, 0.5f).setEaseInElastic();
     }
 }
